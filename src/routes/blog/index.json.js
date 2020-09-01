79a1e8e0e2d8adef217770458250393f3413107e
Riza Fahmi <rizafahmi@gmail.com>
2020-09-02 06:10:02 +0700

    Insert finish!

--- src/routes/blog/index.json.js
+++ src/routes/blog/index.json.js
@@ -15,3 +15,23 @@ export async function get(req, res) {
     res.send({ err });
   }
 }
+
+export async function post(req, res) {
+  try {
+    const db = connect();
+    const { title, slug, html } = req.body;
+    console.log(req.body);
+    const { id } = await db.one(
+      `INSERT INTO posts (title, slug, html) VALUES('${title}', '${slug}', '${html}') returning id`
+    );
+    res.writeHead(200, {
+      'Content-Type': 'application/json'
+    });
+
+    res.end(JSON.stringify(id));
+  } catch (err) {
+    console.error(err);
+    res.writeHead(500, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(err));
+  }
+}
--- src/routes/blog/new.svelte
+++ src/routes/blog/new.svelte
@@ -1,3 +1,28 @@
+<script>
+  import {goto} from '@sapper/app';
+
+  let title, slug, html;
+  async function handleSubmit() {
+    const post = {title, slug, html};
+    try {
+    const response = await fetch(`/blog.json`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(post)
+    });
+      if(response.ok) {
+        const id = await response.json();
+        console.log(id);
+        goto("/blog");
+      }
+
+    } catch(err) {
+      console.error(err);
+    }
+  }
+</script>
 <style>
   input {
     height: 2em;
@@ -27,9 +52,9 @@
 </style>
 
 <h1>New Post</h1>
-<form action="/blog/save">
-  <input type="text" name="title" placeholder="title for your post">
-  <input type="text" name="slug" placeholder="url friendly slug">
-  <textarea id="" name="html" rows="10" ></textarea>
+<form on:submit|preventDefault={handleSubmit}>
+  <input bind:value={title} type="text" name="title" placeholder="title for your post">
+  <input bind:value={slug} type="text" name="slug" placeholder="url friendly slug">
+  <textarea bind:value={html} id="" name="html" rows="10" ></textarea>
   <button type="submit">💾 Save</button>
 </form>
